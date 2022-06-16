3eea6df74a47fb428d2770c2b10cacd95cf5e80c
Jeff S <jeff.shep777@gmail.com>
2022-06-15 19:55:01 -0600

    Add and implement react router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,38 +1,23 @@
-import React, {Component} from 'react'
-import Ideas from '../Ideas/Ideas'
-import Form from '../Form/Form'
+import React from 'react'
+import {Routes,Route} from 'react-router-dom'
+import IdeaBox from '../IdeaBox/IdeaBox'
 import './App.css'
 
-class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      ideas:[
-        { id: 1, title: 'Prank Travis', description: 'Stick googly eyes on all his stuff' },
-        { id: 2, title: 'Make a secret password app', description: 'So you and your rideshare driver can both know neither one of you is lying' },
-        { id: 3, title: 'Learn a martial art', description: 'To exact vengeance upon my enemies' }
-      ]
-    }
-  }
 
-  addIdea = (newIdea) => {
-    this.setState({ideas:[...this.state.ideas,newIdea]})
-  }
 
-  deleteIdea = (id) => {
-    const filteredIdeas = this.state.ideas.filter(idea => idea.id !== id)
-    this.setState({ideas: filteredIdeas})
-  }
+const App = () => {
 
-  render() {
-    return (
-      <main className='App'>
-        <h1>IdeaBox</h1>
-        <Form addIdea={this.addIdea}/>
-        <Ideas deleteIdea={this.deleteIdea} ideas={this.state.ideas}/>
-      </main>
-    )
-  }
+
+  return (
+    <main>
+      <Routes>
+        <Route path='/' element={<IdeaBox />}/>
+      </Routes>
+    </main>
+
+  )
 }
+
+
 
 export default App
