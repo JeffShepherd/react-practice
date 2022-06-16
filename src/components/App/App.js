569d7c76085dc9ac6e67157f2cf1f1bb20325611
Jeff S <jeff.shep777@gmail.com>
2022-06-15 19:13:48 -0600

    Add unique component folders and remove unused assets

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
-import Ideas from './Ideas'
-import Form from './Form'
+import Ideas from '../Ideas/Ideas'
+import Form from '../Form/Form'
 import './App.css'
 
 class App extends Component {
