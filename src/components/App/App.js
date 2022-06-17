f11e5dea9acdd08f1966feeb71ccbece7f155d37
Jeff S <jeff.shep777@gmail.com>
2022-06-17 16:26:08 -0600

    Add home page with link to different apps

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,25 +3,22 @@ import {Routes,Route} from 'react-router-dom'
 import IdeaBox from '../IdeaBoxApp/IdeaBox/IdeaBox'
 import ReadingList from '../ReadingListApp/ReadingList/ReadingList'
 import CryptoPrices from '../CryptoPricesApp/CryptoPrices/CryptoPrices'
+import HomePage from '../HomePage/HomePage'
 import './App.css'
 
-
-
 const App = () => {
-
 
   return (
     <main>
       <Routes>
-        <Route path='/' element={<CryptoPrices />}/>
-        <Route path='/ReadingList' element={<ReadingList />}/>
-        <Route path='/IdeaBox' element={<IdeaBox />}/>
+        <Route path='/' element={<HomePage />}/>
+        <Route path='/cryptoprices' element={<CryptoPrices />}/>
+        <Route path='/readinglist' element={<ReadingList />}/>
+        <Route path='/ideabox' element={<IdeaBox />}/>
       </Routes>
     </main>
 
   )
 }
-
-
 
 export default App
